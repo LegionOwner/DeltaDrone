c0eef56ec7613b5481704598c332808dae3b9306
LegionOwner <choropita.t@gmail.com>
2025-10-31 20:20:39 +0200

    Remove map initialization code from game.js

--- game.js
+++ game.js
@@ -1,5 +1,1 @@
-const map = L.map('radar').setView([50.45, 30.52], 10); // Київ
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-}).addTo(map);
 
